b82d98157f23527db55ff34991872eb6f38410db
LeeJunSeok <wt358@naver.com>
2021-07-14 09:36:30 +0900

    query with dataframe and sql

--- mapreduces/clever-query-mongo.py
+++ mapreduces/clever-query-mongo.py
@@ -46,8 +46,6 @@ if __name__ == "__main__":
     )
     sq.sparkContext.setLogLevel(args.loglevel)
     today = date.today()
-    def cal_age(born):
-        return today.year - born.year - ((today.month, today.day) < (born.month, born.day)).cast("int")
     ts0 = timeit.default_timer()
     if os.path.exists("./patients"):
         pat_df0 = (
@@ -90,13 +88,10 @@ if __name__ == "__main__":
 
     ts1 = timeit.default_timer()
     print("loadtime = {}".format(ts1-ts0))
-    pat_df0.printSchema()
-    print(pat_df0.count())
     pat_df0=pat_df0.withColumn("patientAge",(datediff(current_date(),col("birth"))/365.25).cast(IntegerType()))
-    pat_df0=pat_df0.withColumn("patientGender",pat_df0["sex"])
-    pat_df0.printSchema()
-    pat_df0.orderBy("patientAge",asceding=False).show(truncate=False)
-    df1=pat_df0.withColumn("newage",expr(
+    pat_df0=pat_df0.withColumn("patientGender",pat_df0["sex"]).drop("sex")
+    pat_df0.orderBy("patientAge",asceding=True).show(truncate=False)
+    df0=pat_df0.withColumn("newage",expr(
         "case when patientAge<10 then '0~10'"
         +"when patientAge<20 then '10~20'"
         +"when patientAge<30 then '20~30'"
@@ -108,61 +103,56 @@ if __name__ == "__main__":
         +"when patientAge<90 then '80~90'"
         +"when patientAge<100 then '90~100'"
         +"else 'Very Old' end"
-        )).cube("newage","patientGender").count().sort("newage","patientGender")
+        ))#df0는 환자정보에서 나이만 10로 끊어서
+    df0.show()
+    df1=df0.cube("newage","patientGender").count().sort("newage","patientGender")
     print("각 연령, 성별 등 방문 인원 수")
     df1.show()
-    df1.explain()
-    df_join0=pat_df0.join(trt_df0,"patient")
+    df_join0=df0.join(trt_df0,"patient")
     df_join0.printSchema()
-    df_join0=df_join0.drop("birth","sex").distinct()
-    
+    df_join0=df_join0.distinct()
     df_join0.show()
-    df2=df_join0.withColumn("newage",expr(
-        "case when patientAge<10 then '0~10'"
-        +"when patientAge<20 then '10~20'"
-        +"when patientAge<30 then '20~30'"
-        +"when patientAge<40 then '30~40'"
-        +"when patientAge<50 then '40~50'"
-        +"when patientAge<60 then '50~60'"
-        +"when patientAge<70 then '60~70'"
-        +"when patientAge<80 then '70~80'"
-        +"when patientAge<90 then '80~90'"
-        +"when patientAge<100 then '90~100'"
-        +"else 'Very Old' end"
-        )).cube("newage","patientGender","name").count().sort("newage","patientGender",col("count").desc())
+    df2=df_join0.cube("newage","patientGender","name").count().sort("newage","patientGender",col("count").desc())
     print("각 연령, 성별 받은 진료 수")
     df2.show()
-    df2.explain(False)
     
-    #이 부분은 parquet_faker1이라는 저장소가 존재해야됨.  parquet_faker1은 receipt data
-    ''' 
-    rec_df0=rec_df0.select("treatmentName","treatmentPrice")
-    df3=df_join0.withColumn("newage",expr(
-        "case when patientAge<10 then '0~10'"
-        +"when patientAge<20 then '10~20'"
-        +"when patientAge<30 then '20~30'"
-        +"when patientAge<40 then '30~40'"
-        +"when patientAge<50 then '40~50'"
-        +"when patientAge<60 then '50~60'"
-        +"when patientAge<70 then '60~70'"
-        +"when patientAge<80 then '70~80'"
-        +"when patientAge<90 then '80~90'"
-        +"when patientAge<100 then '90~100'"
-        +"else 'Very Old' end"
-        )).join(rec_df0,rec_df0['treatmentName']==df0['treatmentName']).drop(rec_df0['treatmentName']).distinct().cube("newage","patientGender","treatmentName").agg({"treatmentPrice":"sum"}).sort("newage","patientGender",col("sum(treatmentPrice)").desc())
+    df3=df_join0.cube("newage","patientGender","name").agg({"price":"sum"}).sort("newage","patientGender",col("sum(price)").desc())
     
-    #df3.show()
+    df3.show()
+    
     window=Window.partitionBy(df2['newage'],df2['patientGender']).orderBy(df2['newage'],df2['patientGender'],df2['count'].desc())
     print("각 연령, 성별 많이 받은 진료 탑 3")
-    df2.join(rec_df0,rec_df0['treatmentName']==df2['treatmentName']).distinct().filter(col('newage').isNotNull()).filter(col('patientGender').isNotNull()).filter(df2['treatmentName'].isNotNull()).select('*',rank().over(window).alias('rank')).filter(col('rank')<=3).sort("newage","patientGender",col("count").desc()).show(120,truncate=False)
-    window=Window.partitionBy(df3['newage'],df3['patientGender']).orderBy(df3['newage'],df3['patientGender'],df3['sum(treatmentPrice)'].desc())
+    df4=df2.filter(col('newage').isNotNull()).filter(col('patientGender').isNotNull()).filter(df2['name'].isNotNull()).select('*',rank().over(window).alias('rank')).filter(col('rank')<=3).sort("newage","patientGender",col("count").desc())
+    df4.show()
+    
+    window=Window.partitionBy(df3['newage'],df3['patientGender']).orderBy(df3['newage'],df3['patientGender'],df3['sum(price)'].desc())
     print("각 연령, 성별 가장 많은 매출을 낸 진료 탑 3")
-    df3=df3.filter(col("newage").isNotNull()).filter(col("patientGender").isNotNull()).filter(col("treatmentName").isNotNull()).select('*',rank().over(window).alias('rank')).filter(col('rank')<=3).sort("newage","patientGender",col("sum(treatmentPrice)").desc())
-    df3.show(120,False)
-    df3.explain(False)
-    '''
+    df5=df3.filter(col("newage").isNotNull()).filter(col("patientGender").isNotNull()).filter(col("name").isNotNull()).select('*',rank().over(window).alias('rank')).filter(col('rank')<=3).sort("newage","patientGender",col("sum(price)").desc())
+    df5.show(120,False)
+    
+    df_join1=rec_df0.join(trt_df0,"patient").where(rec_df0["date"]==trt_df0["date"]).where(rec_df0["exiting"]=="1").distinct().groupBy("name").count().orderBy("count",ascending=False)
+    df_join1.show(120,False)
 
-
+    df_join2=rec_df0.join(trt_df0,"patient").where(rec_df0["date"]==trt_df0["date"]).where(rec_df0["exiting"]=="1").distinct().groupBy("name").agg({"price":"sum"}).orderBy("sum(price)",ascending=False)
+    df_join2.show(120,False)
     ts1 = timeit.default_timer()
+    print("df0 explain(환자 나이 계산)")
+    df0.explain()
+    print("df1 explain(각 연령, 성별별로 방문 인원)")
+    df1.explain()
+    print("df2 explain(각 연령, 성별별로 받은 진료 수)")
+    df2.explain(True)
+    print("df_join0 explain(join with treatment)")
+    df_join0.explain()
+    print("df3 explain(각 연령, 성별별 매출)")
+    df3.explain()
+    print("df4 explain(각 연령, 성별별로 치료 받는 진료 탑 3)")
+    df4.explain()
+    print("df5 explain(각 연령, 성별별로 매출 탑 3 진료)")
+    df5.explain(False)
+    print("df5 explain(처음 방문한 사람이 받은 진료 랭킹)")
+    df_join1.explain(False)
+    print("df5 explain(처음방문한 사람이 받은 진료 매출 랭킹)")
+    df_join2.explain(False)
 
     print("runtime={}".format(ts1 - ts0))
