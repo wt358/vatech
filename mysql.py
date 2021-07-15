1267dd1b087d136aef24fed29b4bc7e324ce77bc
Baek, Song Hyun <cmcm0012@gmail.com>
2021-07-15 11:57:58 +0900

    mysql jdbc test

---
+++ mysql.py
@@ -0,0 +1,48 @@
+# spark-submit --driver-class-path=path/to/mysql-connector-java-8.0.25.jar mysql.py
+# $SPARK_HOME/jars에 jdbc connector 파일 넣어두기
+
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
+import timeit
+spark = SparkSession.builder\
+  .appName("Mysql JDBC")\
+  .config("spark.driver.extraClassPath",
+    "$SPARK_HOME/jars/mysql-connector-java-8.0.25.jar")\
+  .getOrCreate()
+
+spark.sparkContext.setLogLevel("ERROR")
+
+sql_url = "localhost"
+user = "spark"
+password = "spark"
+database = "sql_study"
+table = "test"
+
+jdbc = spark.read.format("parquet").load("./parsed/parquet")
+jdbc.show()
+
+start = timeit.default_timer()
+
+jdbc.write.format("jdbc")\
+  .option("driver", "com.mysql.cj.jdbc.Driver")\
+  .option("url", "jdbc:mysql://{}:3306/{}?serverTimezone=Asia/Seoul".format(sql_url, database))\
+  .option("user", user)\
+  .option("password", password)\
+  .option("dbtable", table)\
+  .mode("overwrite")\
+  .save()
+
+end = timeit.default_timer()
+
+print(str(end - start) + " time.")
+
+# upload to mysql cluster -> took about 8 minutes
+# jdbc.write.format("jdbc")\
+#   .option("driver", "com.mysql.cj.jdbc.Driver")\
+#   .option("url", "jdbc:mysql://mysql.k8s.com:3306/faker?serverTimezone=Asia/Seoul")\
+#   .option("user", "root")\
+#   .option("password", "haru1004")\
+#   .option("dbtable", "dummy_clever")\
+#   .mode("overwrite")\
+#   .save()
+
