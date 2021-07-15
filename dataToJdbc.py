afafe0942408bebd44857ea3ac51bff7bad0181d
Baek, Song Hyun <cmcm0012@gmail.com>
2021-07-15 12:00:06 +0900

    Error Occurs

--- dataToJdbc.py
+++ dataToJdbc.py
@@ -14,12 +14,10 @@ df = spark.read.format("parquet").load("./parsed/parquet")
 print("Parsed Data")
 df.show()
 
-# sqlCtx=HiveContext(spark.sparkContext)
-# df.getOrCreateTempView("Dummy")
-
 db_url = "jdbc:hive2://localhost:10000/default"
 my_properties = {
   "user" : "songhyun"
 }
 
-df.write.format("jdbc").option("url", db_url).option("dbtable", "dummy").save()
+# df.write.format("jdbc").option("url", db_url).option("dbtable", "dummy").option("createTableColumnTypes", '''date string, payment string, treat_name string, price BIGINT, uuid4 string, patient_name string, age INTEGER, sex string, dentist string, clinic_name string, address string, telephone string''').save()
+df.write.format("jdbc").option("url", db_url).option("dbtable", "Working").mode("overwrite").save()
