996c95d8bb9ab78f0c15adc35c80600126f3a9f2
Sonins <gmlrhks95@naver.com>
2021-07-19 15:54:29 +0900

    Update install-mongodb-sts.sh

--- k8s-cluster/mongodb/install-mongodb-sts.sh
+++ k8s-cluster/mongodb/install-mongodb-sts.sh
@@ -7,5 +7,4 @@ kubectl config set-context --current --namespace=mongodb
 kubectl apply -f deploy/rbac.yaml -n mongodb
 kubectl apply -f deploy/secrets.yaml -n mongodb
 kubectl apply -f deploy/operator.yaml -n mongodb
-helm install monitoring percona/pmm-server --set platform=kubernetes --version 2.12.0 --set "credentials.password=haru1004"
 kubectl apply -f deploy/cr.yaml -n mongodb
